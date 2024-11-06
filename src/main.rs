e743a6e277455456c3c40820198e18f525c3bc32
pretzelogical <yingyangyo266@gmail.com>
2024-11-06 17:04:32 -0600

    basic config structure without arg parsing or toml parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+// use clap::Parser;
+// use serde::{Serialize, Deserialize};
+// use toml;
+
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::build();
+    println!("search_dirs = {:#?}", config.config_file.search_dirs);
+    println!("target = {}", config.target);
+}
+
+#[derive(Debug)]
+struct Config {
+    config_file: Box<ConfigFile>,
+    target: String,
+}
+
+impl Config {
+    fn build() -> Config {
+        Config {
+            config_file: Box::new(ConfigFile::build()),
+            target: String::from("pgadmin4")
+        }
+    }
+}
+
+#[derive(Debug)]
+struct ConfigFile {
+    search_dirs: Vec<String>
+}
+
+impl ConfigFile {
+    fn build() -> ConfigFile {
+        ConfigFile {
+            search_dirs: vec![String::from("/usr/bin/applications")]
+        }
+    }
 }
