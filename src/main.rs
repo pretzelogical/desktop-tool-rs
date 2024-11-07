31d882bf968f1eff14750da2acfd8e4d88cab5c1
pretzelogical <yingyangyo266@gmail.com>
2024-11-06 22:35:53 -0600

    merge cli config and fake file config

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,29 @@
-// use clap::Parser;
 // use serde::{Serialize, Deserialize};
 // use toml;
 
 
 fn main() {
     let config = dtconfig::Config::build();
-    match config.target {
-        Some(target) => println!("target = {target}"),
-        _ => ()
-    }
-    match config.search_dirs {
-        Some(search_dirs) => println!("search_dirs = {:?}", search_dirs),
-        _ => ()
-    }
+    println!("final config = {:?}", config);
 }
 
 mod dtconfig {
     use merge::Merge;
-    // use regex::Regex;
     use std::env;
 
     #[derive(Debug, Clone, Merge)]
     pub struct Config {
         pub target: Option<String>,
-        pub search_dirs: Option<Vec<String>>
+
+        #[merge(strategy = merge::vec::append)]
+        pub search_dirs: Vec<String>
     }
 
     impl Default for Config {
         fn default() -> Config {
             Config {
-                target: Some(String::from("pgadmin4")),
-                search_dirs: Some(vec![String::from("/usr/share/applications")])
+                target: None,
+                search_dirs: Vec::new()
             }
         }
     }
@@ -38,16 +31,16 @@ mod dtconfig {
     impl Config {
         pub fn build() -> Config {
             let cli_config = Config::from_cli();
-            // let file_config = Config::from_file();
+            let mut file_config = Config {
+                target: Some(String::from("pgadmin4")),
+                search_dirs: vec![String::from("/usr/share/applications")]
+            };
 
             println!("cli config = {:?}", cli_config);
 
-            Config {
-                target: Some(String::from("pgadmin4")),
-                search_dirs: Some(vec![String::from("/usr/share/applications")])
-            }
+            file_config.merge(cli_config);
+            file_config
         }
-
 
         fn from_cli() -> Config {
             let args = env::args().skip(1);
@@ -66,7 +59,7 @@ mod dtconfig {
                     println!("option arg = {option_arg}");
                     match option_arg.as_str() {
                         "--search-dirs" => search_dirs.push(arg.clone()),
-                        _ => println!("Error parsing option arguments")
+                        _ => println!("Invalid option argument")
                     }
                     is_option = false;
                 }
@@ -74,7 +67,9 @@ mod dtconfig {
                 // Is the target
                 config.target = Some(arg.clone());
             }
-            config.search_dirs = Some(search_dirs);
+            if search_dirs.len() > 0 {
+                config.search_dirs = search_dirs;
+            }
             config
         }
     }
