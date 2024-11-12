a069ce8d2eb6c51f7f8cd4fb1d63fe586895af91
pretzelogical <yingyangyo266@gmail.com>
2024-11-11 23:37:21 -0600

    search dirs for files

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 mod dt_config;
+use std::panic::panic_any;
+
 use dt_config::Config;
 
 
 fn main() {
     let config = Config::build();
     println!("final config = {:?}", config);
+    search_for_file(&config);
 }
 
-// fn search_for_file(config: &Config) -> std::io::Result<String> {
-//     for dir in config.search_dirs {
-        
-//     }
-// }
+fn search_for_file(config: &Config) {
+    for path in &config.search_dirs {
+        let dir = std::fs::read_dir(path)
+            .expect("Error reading path");
+
+        for file in dir {
+            match file {
+                Ok(file) => println!("{:?}", file),
+                Err(error) => panic_any(error.to_string())
+            }
+        }
+    }
+}
