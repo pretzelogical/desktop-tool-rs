c940f937a7108fff397d56b79de3e1097eb01a24
pretzelogical <yingyangyo266@gmail.com>
2024-11-11 23:02:04 -0600

    nix the config.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-mod config;
-use config::Config;
+mod dt_config;
+use dt_config::Config;
+
 
 fn main() {
     let config = Config::build();
     println!("final config = {:?}", config);
 }
+
+// fn search_for_file(config: &Config) -> std::io::Result<String> {
+//     for dir in config.search_dirs {
+        
+//     }
+// }
