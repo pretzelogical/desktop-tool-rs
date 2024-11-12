b3291d6e7b5072d00535993078b32e82adb89bee
pretzelogical <yingyangyo266@gmail.com>
2024-11-12 00:02:01 -0600

    search and locate desktop file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod dt_config;
-use std::panic::panic_any;
+use std::{fs::DirEntry, panic::panic_any};
 
 use dt_config::Config;
 
@@ -11,15 +11,37 @@ fn main() {
 }
 
 fn search_for_file(config: &Config) {
+    let target= config.target
+        .as_ref()
+        .expect("Error parsing target!");
     for path in &config.search_dirs {
         let dir = std::fs::read_dir(path)
             .expect("Error reading path");
 
-        for file in dir {
-            match file {
-                Ok(file) => println!("{:?}", file),
+        for file_path in dir {
+            match file_path {
+                Ok(file_path) => {
+                    if let Some(_file) = check_file(file_path, target) {
+                        return;
+                    }
+                },
                 Err(error) => panic_any(error.to_string())
             }
         }
     }
+}
+
+fn check_file(file_path: DirEntry, target: &String) -> Option<DirEntry> {
+    println!("{:?}", file_path);
+    let file_name_os = file_path
+        .file_name();
+    let file_name = file_name_os
+        .into_string()
+        .expect("Error parsing dir!");
+
+    if file_name.ends_with(".desktop") && file_name.contains(target) {
+        println!("Found target {target}");
+        return Some(file_path);
+    }
+    return None;
 }
